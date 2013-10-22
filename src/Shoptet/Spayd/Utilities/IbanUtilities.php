4fa53854db36ec1930bb6ebc38508064f6a1b52f
Tomas Lebeda <lebeda@cybergenics.cz>
2013-10-22 17:39:36 +0200

    Return FALSE when account is not valid

--- src/Shoptet/Spayd/Utilities/IbanUtilities.php
+++ src/Shoptet/Spayd/Utilities/IbanUtilities.php
@@ -11,12 +11,14 @@ class IbanUtilities {
 			$prefix = sprintf('%06d', $czechAccount->getPrefix());
 			$accountNumber = sprintf('%010d', $czechAccount->getAccountNumber());
 			$bankCode = sprintf('%04d', $czechAccount->getBankCode());
+			
+			$accountBuffer = $bankCode . $prefix . $accountNumber . $czechAccount->getNumericLanguageCode();
+			$checksum = sprintf('%02d', (98 - bcmod($accountBuffer, 97)));
+
+			// build the IBAN number
+			return 'CZ' . $checksum . $bankCode . $prefix . $accountNumber;
+		} else {
+			return FALSE;
 		}
-		
-		$accountBuffer = $bankCode . $prefix . $accountNumber . $czechAccount->getNumericLanguageCode();
-		$checksum = sprintf('%02d', (98 - bcmod($accountBuffer, 97)));
-		
-		// build the IBAN number
-		return 'CZ' . $checksum . $bankCode . $prefix . $accountNumber;
 	}
 }
