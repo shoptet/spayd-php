ec983cbe627a7711a9f4836eedc790826d9db3ca
Tomas Lebeda <lebeda@cybergenics.cz>
2013-10-22 18:29:14 +0200

    Removed overwrited property

--- src/Shoptet/Spayd/Model/CzechAccount.php
+++ src/Shoptet/Spayd/Model/CzechAccount.php
@@ -7,7 +7,6 @@ class CzechAccount extends DefaultAccount {
 	const PREFIX_DELIMITER = '-';
 	const BANK_CODE_DELIMITER = '/';
 	
-	private $accountString;
 	private $czechRegularExpression = '~^(?P<prefixPart>(?P<prefix>\d{0,6})(?P<prefixDelimiter>\-{1})){0,1}(?P<accountPart>(?P<accountNumber>\d{2,10})(?P<bankCodeDelimiter>\/{1})(?P<bankCode>\d{3,4}))$~';
 	
 	private $prefix;
