7aa6458efcc58651170624e153c08f1fa0ae9a57
Tomas Lebeda <lebeda@cybergenics.cz>
2013-10-23 13:56:26 +0200

    Slovakia account model class

---
+++ src/Shoptet/Spayd/Model/SlovakiaAccount.php
@@ -0,0 +1,9 @@
+<?php
+namespace Shoptet\Spayd\Model;
+use Shoptet\Spayd\Model;
+
+class SlovakiaAccount extends CzechAccount {
+
+	protected $regularExpression = '~^(?P<prefixPart>(?P<prefix>\d{0,6})(?P<prefixDelimiter>\-{1})){0,1}(?P<accountPart>(?P<accountNumber>\d{2,10})(?P<bankCodeDelimiter>\/{1})(?P<bankCode>\d{3,4}|[A-Z]{4,7}))$~';
+	
+}
