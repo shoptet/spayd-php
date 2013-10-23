903df505a0a25103a76e1be3b8863b5b12902cb3
lebeda <lebeda@LEBEDA.shoptet.local>
2013-10-23 13:58:06 +0200

    Computing IBAN from Account was normalized standardized

--- src/Shoptet/Spayd/Utilities/IbanUtilities.php
+++ src/Shoptet/Spayd/Utilities/IbanUtilities.php
@@ -6,17 +6,17 @@ use Shoptet\Spayd\Exceptions;
 
 class IbanUtilities {
 	
-	public static function computeIBANFromCzechBankAccount(\Shoptet\Spayd\Model\CzechAccount $czechAccount) {
-		if ($czechAccount->isValid()) {
-			$prefix = sprintf('%06d', $czechAccount->getPrefix());
-			$accountNumber = sprintf('%010d', $czechAccount->getAccountNumber());
-			$bankCode = sprintf('%04d', $czechAccount->getBankCode());
+	public static function computeIbanFromBankAccount(\Shoptet\Spayd\Model\DefaultAccount $account) {
+		if ($account->isValid()) {
+			$prefix = sprintf('%06d', $account->getPrefix());
+			$accountNumber = sprintf('%010d', $account->getAccountNumber());
+			$bankCode = sprintf('%04d', $account->getBankCode());
 			
-			$accountBuffer = $bankCode . $prefix . $accountNumber . $czechAccount->getNumericLanguageCode();
+			$accountBuffer = $bankCode . $prefix . $accountNumber . $account->getNumericLanguageCode();
 			$checksum = sprintf('%02d', (98 - bcmod($accountBuffer, 97)));
 
 			// build the IBAN number
-			return 'CZ' . $checksum . $bankCode . $prefix . $accountNumber;
+			return $account->getLocale() . $checksum . $bankCode . $prefix . $accountNumber;
 		} else {
 			return FALSE;
 		}
